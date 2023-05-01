18759eacdb9837eb59c3fa07c199565d59ad4944
Militaru Andreea <andreea.mili20@gmail.com>
2023-05-01 17:36:59 +0300

    trying

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,8 +5,15 @@
  function Home() {
     
    return (
-     <div>
+     <div className='home'>
        <img className='home-image' src={homeImage} alt='Home Background' />
+          <p className='text-align '>
+       Welcome to the CSM Equestrian Club Sibiu website! We are a passionate community of horse lovers dedicated to improving
+        our skills and participating in equestrian competitions and events both in Romania and abroad. We pride ourselves on
+        having a team of experienced coaches who are dedicated to developing you as a rider, regardless of your current level
+        of experience.</p>
+
+
      </div>
    );
  }
